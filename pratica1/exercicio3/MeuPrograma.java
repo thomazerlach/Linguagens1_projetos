4a781fb11443a5a68f826736a6b9ce7356ce320b
thomazerlach <thomazerlach@hotmail.com>
2017-03-08 09:46:46 -0300

    Add exercicio3

---
+++ pratica1/exercicio3/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma {
+  public static void main(String[] args) {
+    // Mensagem de saida do sistema
+    System.out.println("Olá mundo!\nEstou programando no modo Conan. :)");
+
+  }
+}
