0ee824fef12194df7fec29e26de21c4407f8693a
thomazerlach <thomazerlach@hotmail.com>
2017-08-29 10:54:17 -0300

    Updated pratica7

--- pratica7/Celular/src/celular/FXMLDocumentController.java
+++ pratica7/Celular/src/celular/FXMLDocumentController.java
@@ -15,11 +15,15 @@ import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
 /**
  *
  * @author thomazerlach
@@ -46,98 +50,135 @@ public class FXMLDocumentController implements Initializable {
     private TextField txtNumero;
     private final ListaDeContatos listaCompleta = new ListaDeContatos();
     private final ObservableList<Contato> contatoData = FXCollections.observableArrayList();
-    
+    private Stage dialogStage;
+
     public void ButtonAdd(ActionEvent event) {
-        listaCompleta.AddNaLista(txtAdicionar.getText(), txtNumero.getText());
-        txtAdicionar.setText("");
-        txtNumero.setText("");
-        List lst = listaCompleta.getLista();
-        contatoData.clear();
-        for (int i = 0; i < lst.size(); i++) {
-            Contato ct = (Contato) lst.get(i);
-            contatoData.add(ct);
-        }
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
-        FilteredList<Contato> filteredData = new FilteredList<>(contatoData, p -> true);
-
-        // 2. Set the filter Predicate whenever the filter changes.
-        txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(contato -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                if (contato.getNome().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else if (contato.getNumero().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches last name.
-                }
-                return false; // Does not match.
-            });
-        });
-        
-        // 3. Wrap the FilteredList in a SortedList. 
-        SortedList<Contato> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableContato.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
-        tableContato.setItems(sortedData);
+        this.Adicionar();
     }
-    
+
     public void ButtonRemover(ActionEvent event) {
-        listaCompleta.Remover(txtRemover.getText());
-        txtRemover.setText("");
-        List lst = listaCompleta.getLista();
-        contatoData.clear();
-        for (int i = 0; i < lst.size(); i++) {
-            Contato ct = (Contato) lst.get(i);
-            contatoData.add(ct);
-        }
-        
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
-        FilteredList<Contato> filteredData = new FilteredList<>(contatoData, p -> true);
-
-        // 2. Set the filter Predicate whenever the filter changes.
-        txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(contato -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                if (contato.getNome().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else if (contato.getNumero().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches last name.
-                }
-                return false; // Does not match.
-            });
-        });
-        
-        // 3. Wrap the FilteredList in a SortedList. 
-        SortedList<Contato> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableContato.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
-        tableContato.setItems(sortedData);
+        this.Remover();
     }
-    
+
+    public void Adicionar() {
+        if (txtNumero.getText().equalsIgnoreCase("") || txtAdicionar.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Campos Inválidos");
+            alert.setHeaderText("Preencha todos os campos!");
+            alert.showAndWait();
+        } else {
+            listaCompleta.AddNaLista(txtAdicionar.getText(), txtNumero.getText());
+            txtAdicionar.setText("");
+            txtNumero.setText("");
+            List lst = listaCompleta.getLista();
+            contatoData.clear();
+            for (int i = 0; i < lst.size(); i++) {
+                Contato ct = (Contato) lst.get(i);
+                contatoData.add(ct);
+            }
+            // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+            FilteredList<Contato> filteredData = new FilteredList<>(contatoData, p -> true);
+
+            // 2. Set the filter Predicate whenever the filter changes.
+            txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(contato -> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (contato.getNome().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (contato.getNumero().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches last name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            // 3. Wrap the FilteredList in a SortedList. 
+            SortedList<Contato> sortedData = new SortedList<>(filteredData);
+
+            // 4. Bind the SortedList comparator to the TableView comparator.
+            sortedData.comparatorProperty().bind(tableContato.comparatorProperty());
+
+            // 5. Add sorted (and filtered) data to the table.
+            tableContato.setItems(sortedData);
+        }
+    }
+
+    public void Remover() {
+        if (txtRemover.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Campos Inválidos");
+            alert.setHeaderText("Preencha todos os campos!");
+            alert.showAndWait();
+        } else {
+            listaCompleta.Remover(txtRemover.getText());
+            txtRemover.setText("");
+            List lst = listaCompleta.getLista();
+            contatoData.clear();
+            for (int i = 0; i < lst.size(); i++) {
+                Contato ct = (Contato) lst.get(i);
+                contatoData.add(ct);
+            }
+
+            // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+            FilteredList<Contato> filteredData = new FilteredList<>(contatoData, p -> true);
+
+            // 2. Set the filter Predicate whenever the filter changes.
+            txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(contato -> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (contato.getNome().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (contato.getNumero().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches last name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            // 3. Wrap the FilteredList in a SortedList. 
+            SortedList<Contato> sortedData = new SortedList<>(filteredData);
+
+            // 4. Bind the SortedList comparator to the TableView comparator.
+            sortedData.comparatorProperty().bind(tableContato.comparatorProperty());
+
+            // 5. Add sorted (and filtered) data to the table.
+            tableContato.setItems(sortedData);
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         columnNome.setCellValueFactory(new PropertyValueFactory<Contato, String>("nome"));
         columnNumero.setCellValueFactory(new PropertyValueFactory<Contato, String>("numero"));
-    }    
-    
+        txtNumero.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                // do what is to do
+                this.Adicionar();
+            }
+        });
+        txtRemover.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                // do what is to do
+                this.Remover();
+            }
+        });
+    }
+
 }
--- pratica7/ExercicioCompras/src/exerciciocompras/FXMLDocumentController.java
+++ pratica7/ExercicioCompras/src/exerciciocompras/FXMLDocumentController.java
@@ -15,18 +15,21 @@ import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
 
 /**
  *
  * @author thomazerlach
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private TextField txtPesquisar;
     @FXML
@@ -51,141 +54,196 @@ public class FXMLDocumentController implements Initializable {
     private TextField txtIndex;
     private final ListaDeCompras listaCompleta = new ListaDeCompras();
     private final ObservableList<Compra> compraData = FXCollections.observableArrayList();
-    
+    private Stage dialogStage;
+
     public void ButtonAdd(ActionEvent event) {
-        listaCompleta.AddNaLista(txtAdd.getText());
-        txtAdd.setText("");
-        List lst = listaCompleta.getLista();
-        compraData.removeAll(lst);
-        for (int i = 0; i < lst.size(); i++) {
-            Compra cp = (Compra) lst.get(i);
-            compraData.add(cp);
-        }
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
-        FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
-
-        // 2. Set the filter Predicate whenever the filter changes.
-        txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(compra -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches last name.
-                }
-                return false; // Does not match.
-            });
-        });
-
-        // 3. Wrap the FilteredList in a SortedList. 
-        SortedList<Compra> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
-        tableLista.setItems(sortedData);
+        this.Adicionar();
     }
-    
+
     public void ButtonRemover(ActionEvent event) {
-        listaCompleta.Remover(txtRemover.getText());
-        txtRemover.setText("");
-        List lst = listaCompleta.getLista();
-        compraData.clear();
-        for (int i = 0; i < lst.size(); i++) {
-            Compra cp = (Compra) lst.get(i);
-            compraData.add(cp);
-        }
-        
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
-        FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
-
-        // 2. Set the filter Predicate whenever the filter changes.
-        txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(compra -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches last name.
-                }
-                return false; // Does not match.
-            });
-        });
-
-        // 3. Wrap the FilteredList in a SortedList. 
-        SortedList<Compra> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
-        tableLista.setItems(sortedData);
+        this.Remover();
     }
-    
+
     public void ButtonModify(ActionEvent event) {
-        listaCompleta.Modificar(txtModificar.getText(), Integer.valueOf(txtIndex.getText()));
-        txtModificar.setText("");
-        txtIndex.setText("");
-        List lst = listaCompleta.getLista();
-        compraData.clear();
-        for (int i = 0; i < lst.size(); i++) {
-            Compra cp = (Compra) lst.get(i);
-            compraData.add(cp);
-        }
-        
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
-        FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
-
-        // 2. Set the filter Predicate whenever the filter changes.
-        txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(compra -> {
-                // If filter text is empty, display all persons.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();       
-
-                if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches first name.
-                } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
-                    return true; // Filter matches last name.
-                }
-                return false; // Does not match.
-            });
-        });
-
-        // 3. Wrap the FilteredList in a SortedList. 
-        SortedList<Compra> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
-        tableLista.setItems(sortedData);
+        this.Modificar();
     }
-    
+
+    public void Adicionar() {
+        if (txtAdd.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Campos Inválidos");
+            alert.setHeaderText("Preencha todos os campos!");
+            alert.showAndWait();
+        } else {
+            listaCompleta.AddNaLista(txtAdd.getText());
+            txtAdd.setText("");
+            List lst = listaCompleta.getLista();
+            compraData.removeAll(lst);
+            for (int i = 0; i < lst.size(); i++) {
+                Compra cp = (Compra) lst.get(i);
+                compraData.add(cp);
+            }
+            // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+            FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
+
+            // 2. Set the filter Predicate whenever the filter changes.
+            txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(compra -> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches last name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            // 3. Wrap the FilteredList in a SortedList. 
+            SortedList<Compra> sortedData = new SortedList<>(filteredData);
+
+            // 4. Bind the SortedList comparator to the TableView comparator.
+            sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
+
+            // 5. Add sorted (and filtered) data to the table.
+            tableLista.setItems(sortedData);
+        }
+    }
+
+    public void Remover() {
+        if (txtRemover.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Campos Inválidos");
+            alert.setHeaderText("Preencha todos os campos!");
+            alert.showAndWait();
+        } else {
+            listaCompleta.Remover(txtRemover.getText());
+            txtRemover.setText("");
+            List lst = listaCompleta.getLista();
+            compraData.clear();
+            for (int i = 0; i < lst.size(); i++) {
+                Compra cp = (Compra) lst.get(i);
+                compraData.add(cp);
+            }
+
+            // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+            FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
+
+            // 2. Set the filter Predicate whenever the filter changes.
+            txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(compra -> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches last name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            // 3. Wrap the FilteredList in a SortedList. 
+            SortedList<Compra> sortedData = new SortedList<>(filteredData);
+
+            // 4. Bind the SortedList comparator to the TableView comparator.
+            sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
+
+            // 5. Add sorted (and filtered) data to the table.
+            tableLista.setItems(sortedData);
+        }
+    }
+
+    public void Modificar() {
+        if (txtModificar.getText().equalsIgnoreCase("") || txtIndex.getText().equalsIgnoreCase("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Campos Inválidos");
+            alert.setHeaderText("Preencha todos os campos!");
+            alert.showAndWait();
+        } else {
+            listaCompleta.Modificar(txtModificar.getText(), Integer.valueOf(txtIndex.getText()));
+            txtModificar.setText("");
+            txtIndex.setText("");
+            List lst = listaCompleta.getLista();
+            compraData.clear();
+            for (int i = 0; i < lst.size(); i++) {
+                Compra cp = (Compra) lst.get(i);
+                compraData.add(cp);
+            }
+
+            // 1. Wrap the ObservableList in a FilteredList (initially display all data).
+            FilteredList<Compra> filteredData = new FilteredList<>(compraData, p -> true);
+
+            // 2. Set the filter Predicate whenever the filter changes.
+            txtPesquisar.textProperty().addListener((observable, oldValue, newValue) -> {
+                filteredData.setPredicate(compra -> {
+                    // If filter text is empty, display all persons.
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+
+                    // Compare first name and last name of every person with filter text.
+                    String lowerCaseFilter = newValue.toLowerCase();
+
+                    if (compra.getItem().toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches first name.
+                    } else if (Integer.toString(compra.getIndice()).toLowerCase().contains(lowerCaseFilter)) {
+                        return true; // Filter matches last name.
+                    }
+                    return false; // Does not match.
+                });
+            });
+
+            // 3. Wrap the FilteredList in a SortedList. 
+            SortedList<Compra> sortedData = new SortedList<>(filteredData);
+
+            // 4. Bind the SortedList comparator to the TableView comparator.
+            sortedData.comparatorProperty().bind(tableLista.comparatorProperty());
+
+            // 5. Add sorted (and filtered) data to the table.
+            tableLista.setItems(sortedData);
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         columnIndex.setCellValueFactory(new PropertyValueFactory<Compra, String>("indice"));
         columnLista.setCellValueFactory(new PropertyValueFactory<Compra, String>("Item"));
-    }    
-    
+        txtAdd.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                // do what is to do
+                this.Adicionar();
+            }
+        });
+        txtRemover.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                // do what is to do
+                this.Remover();
+            }
+        });
+        txtIndex.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                // do what is to do
+                this.Modificar();
+            }
+        });
+    }
+
 }
